574a2fbfa3b9a693a67396095399ab9e0c4088b2
nullrocket <douglas.b.hill@gmail.com>
2022-04-19 17:03:28 -0400

    Initial commit

---
+++ scripts/delete_files.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+rm -rf ../c/sqlite/art
+rm -rf ../c/sqlite/autoconf/Makefile.msc
+rm -rf ../c/sqlite/autoconf/Makefile.fallback
+rm -rf ../c/sqlite/macosx
+rm -rf ../c/sqlite/doc
+rm -rf ../c/sqlite/vsixtest
+rm -rf ../c/sqlite/CHANGELOG.md
